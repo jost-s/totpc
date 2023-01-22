1b212989cb1d4b4d0b0f2dcda3815fba2010c45e
Jost Schulte <jost.schulte@protonmail.com>
2023-01-22 06:36:05 -0600

    test(integration): remove tests using actual file

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -2,10 +2,7 @@ const BIN: &str = "totp";
 
 mod save {
     use assert_cmd::prelude::*;
-    use std::{
-        io::Write,
-        process::{Command, Stdio},
-    };
+    use std::process::Command;
     use totp::{ErrorMessage, COMMAND_SAVE};
 
     use crate::BIN;
@@ -20,41 +17,5 @@ mod save {
         assert!(error
             .to_string()
             .contains(ErrorMessage::MissingIdentifier.as_str()));
-    }
-
-    #[test]
-    #[ignore = "uses actual file"]
-    fn without_key_fails() {
-        let mut command = Command::cargo_bin(BIN).unwrap();
-        let command = command.arg(COMMAND_SAVE).arg("test_identifier");
-
-        command.assert().code(1);
-        let error = command.unwrap_err();
-        println!("{}", error);
-        assert!(error.to_string().contains(ErrorMessage::EmptyKey.as_str()));
-    }
-
-    #[test]
-    #[ignore = "uses actual file"]
-    fn succeeds() {
-        let mut command = Command::cargo_bin(BIN).unwrap();
-        let mut child = command
-            .arg(COMMAND_SAVE)
-            .arg("test_identifier")
-            .stdin(Stdio::piped())
-            .spawn()
-            .unwrap();
-        // if let Some(mut stdin) = child.stdin.take() {
-        //     stdin.write_all(b"ORSXG5A=").unwrap();
-        // }
-        let child_stdin = child.stdin.as_mut().unwrap();
-        child_stdin.write_all(b"ORSXG5A=").unwrap();
-        let output = child.wait_with_output().unwrap();
-        println!("{:?}", output);
-
-        // command.assert().code(1);
-        // let error = command.unwrap_err();
-        // println!("{error}");
-        // assert!(error.to_string().contains(ErrorMessage::EmptyKey.as_str()));
     }
 }
