17cd1e76b040afee5ca4a06450b4194517fb5e50
Jost Schulte <jost.schulte@protonmail.com>
2023-07-07 20:15:34 -0600

    docs: add docs comments

--- src/base32.rs
+++ src/base32.rs
@@ -1,9 +1,7 @@
 use base32::Alphabet;
 
+/// Decode Base32 encoded key to UTF-8 string.
 pub fn decode(key: &str) -> Result<Vec<u8>, String> {
     let maybe_key_bytes = base32::decode(Alphabet::RFC4648 { padding: false }, key);
-    match maybe_key_bytes {
-        None => Err("Error: invalid key encoding (must be Base32)".to_string()),
-        Some(bytes) => Ok(bytes),
-    }
+    maybe_key_bytes.ok_or_else(|| "Error: invalid key encoding (must be Base32)".to_string())
 }
